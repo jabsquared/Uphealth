c49cd77ad8e9509a07d421df6d168590e5cd68ea
Louis Graham Vichy <lgvichy@gmail.com>
2015-10-12 11:39:19 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,10 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('starter', ['ionic', 'angular-preload-image', 'btford.socket-io', 'ngStorage', 'ngCordova']);
+var app = angular.module('starter', ['ionic', 'angular-preload-image', 'ngStorage', 'ngCordova']);
 
 app.run(function($ionicPlatform, $http) {
-  $http.defaults.headers.common.Authorization = 'Client-ID 61c4d0c39d59115';
   $ionicPlatform.ready(function() {
     // Set default of first time user to true
     // $localStorage.$default({
