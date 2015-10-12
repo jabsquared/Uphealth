ba163994b81c830d301ebdf76c392004a2fc95eb
Louis Graham Vichy <lgvichy@gmail.com>
2015-10-12 11:38:38 -0700

    Update socketService.js

--- js/services/socketService.js
+++ js/services/socketService.js
@@ -1,6 +1,5 @@
 app.factory('socket9', function(socketFactory) {
-  var mis = io.connect('https://9billion.mybluemix.net/');
-  // var mis = io.connect('http://localhost:1314/');
+  var mis = io.connect('http://localhost:1314/');
 
   mySocket = socketFactory({
     ioSocket : mis,
